f4ab6f02ba893236f189d2b20cb5be06eaa4e81e
Eric Alfred <alfredoeric@gmail.com>
2023-09-04 01:49:06 +0100

    first commit

---
+++ static/main.js
@@ -0,0 +1,79 @@
+import { country_list } from './country-list.js';
+
+function populateCountries(element) {
+  for (const currencyCode in country_list) {
+//     console.log(currencyCode)
+     const option = document.createElement('option');
+    option.value = currencyCode;
+    option.text = currencyCode;
+    
+    element.appendChild(option);
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fromCurrency = document.getElementById('fromCurrency');
+  const toCurrency = document.getElementById('toCurrency');
+  const reverseBtn = document.getElementById('reverse');
+  const toFlag = document.getElementById('toFlag');
+  const fromFlag = document.getElementById('fromFlag');
+  
+  reverseBtn.addEventListener('click', () => {
+   const tempCurrency = fromCurrency.value;
+   fromCurrency.value = toCurrency.value;
+   toCurrency.value = tempCurrency;
+    
+     loadFlag(fromCurrency, fromFlag);
+     loadFlag(toCurrency, toFlag);
+  exchange();
+  });
+  
+  fromCurrency.addEventListener("change", () => loadFlag(fromCurrency, fromFlag));
+    toCurrency.addEventListener("change", () => loadFlag(toCurrency, toFlag));
+  
+  populateCountries(fromCurrency);
+  populateCountries(toCurrency);
+  
+});
+
+const convertBtn = document.getElementById('convert');
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  exchange();
+});
+
+function exchange() {
+  const fromCurrency = document.getElementById('fromCurrency').value;
+  const toCurrency = document.getElementById('toCurrency').value;
+  const amount = parseFloat(document.getElementById('amount').value);
+  
+  const apiKey = '2af520af6ae8de39a312f153'
+  const apiUrl =
+  `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}`
+  
+  document.getElementById('loader').style.display = 'inline';
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('loader').style.display = 'none';
+    console.log(data)
+    const result = document.getElementById('result');
+    const exchangeRate = data.conversion_rate;
+    
+    const convertedAmount = (amount * exchangeRate).toFixed(2);
+    result.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+  })
+  .catch(error => {
+     console.error("An error occurred:", error);
+     const resultElement = document.getElementById("result");
+     resultElement.textContent = "Something went wrong. Please try again later.";
+  });
+}
+
+function loadFlag(currencySelect, flagImg) {
+  const currencyCode = currencySelect.value;
+  flagImg.src = `https://flagcdn.com/48x36/${country_list[currencyCode].toLowerCase()}.png`;
+    }
